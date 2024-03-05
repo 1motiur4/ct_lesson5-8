32d76bc12ab5d8e3a24d3afab53cf7dfa9d16be5
1motiur4 <92241810+1motiur4@users.noreply.github.com>
2024-03-04 22:14:08 -0500

    tictactoe done

--- tictactoe.rb
+++ tictactoe.rb
@@ -24,6 +24,18 @@ class Board
   def rows
     @matrix
   end
+
+  def columns
+    @matrix.transpose
+  end
+
+  def diagonal_1
+    [@matrix[0][0], @matrix[1][1], @matrix[2][2]]
+  end
+
+  def diagonal_2
+    [@matrix[2][0], @matrix[1][1], @matrix[0][2]]
+  end
 end
 
 class Game
@@ -48,12 +60,25 @@ class Game
 
       x_turn = !x_turn
     end
+
+    puts "#{winner} is the winner"
   end
 
   def winner
     @board.rows.each do |row|
       return row.first if row.uniq.count == 1 && !row.first.nil?
     end
+
+    @board.columns.each do |column|
+      return column.first if column.uniq.count == 1 && !column.first.nil?
+    end
+
+    diagonal_1 = @board.diagonal_1
+    return diagonal_1.first if diagonal_1.uniq.count == 1 && !diagonal_1.first.nil?
+
+    diagonal_2 = @board.diagonal_2
+    return diagonal_2.first if diagonal_2.uniq.count == 1 && !diagonal_2.first.nil?
+
     false
   end
 end

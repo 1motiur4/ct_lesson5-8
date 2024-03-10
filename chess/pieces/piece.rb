091dbb15996bd414ddabdab5d470079ed38d4dda
1motiur4 <92241810+1motiur4@users.noreply.github.com>
2024-03-10 00:03:53 -0500

    chess pt 3 - split up files

---
+++ chess/pieces/piece.rb
@@ -0,0 +1,16 @@
+class Piece
+  attr_reader :color
+
+  def initialize(color)
+    @color = color
+  end
+
+  def display
+    colorize_color = @color == "white" ? :light_white : :light_black
+    return " #{picture} ".colorize(color: colorize_color)
+  end
+
+  def picture
+    "?"
+  end
+end
---
+++ chess/pieces/rook.rb
@@ -0,0 +1,7 @@
+require_relative "piece"
+
+class Rook < Piece
+  def picture
+    "\u265C"
+  end
+end
